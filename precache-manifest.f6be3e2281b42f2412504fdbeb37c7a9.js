d67c1462057637781402069154e3feb80e58825d
blueLion <bluelion034@gmail.com>
2022-08-30 19:23:34 +0300

    user interface commit one

---
+++ precache-manifest.f6be3e2281b42f2412504fdbeb37c7a9.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b920213379696fe020e22db427ee16bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f01baf521caf7e3ece23",
+    "url": "/static/css/2.18b175e6.chunk.css"
+  },
+  {
+    "revision": "765dc83e43343559b334",
+    "url": "/static/css/main.aa77f9e2.chunk.css"
+  },
+  {
+    "revision": "f01baf521caf7e3ece23",
+    "url": "/static/js/2.174610e7.chunk.js"
+  },
+  {
+    "revision": "d17f685151927f8ec5c2153296bafef0",
+    "url": "/static/js/2.174610e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "765dc83e43343559b334",
+    "url": "/static/js/main.5df743c1.chunk.js"
+  },
+  {
+    "revision": "f5e13adc044e78287340",
+    "url": "/static/js/runtime-main.fb8ad9d3.js"
+  },
+  {
+    "revision": "fa7be6e9fc5130efb34a4879fcee01d6",
+    "url": "/static/media/bold-sale-logo.fa7be6e9.png"
+  },
+  {
+    "revision": "8a50d86349aa58d92c37441b4b727a07",
+    "url": "/static/media/boldsale-litepaper.8a50d863.pdf"
+  },
+  {
+    "revision": "f489fdd2acef80ca9cac16dbed436249",
+    "url": "/static/media/boldsale-whitepaper.f489fdd2.pdf"
+  },
+  {
+    "revision": "1aeef708761941f40cb61d21d24b5f26",
+    "url": "/static/media/bsc-main.1aeef708.svg"
+  },
+  {
+    "revision": "ef75c1df7562c0f95415792836c10636",
+    "url": "/static/media/bsc-main.ef75c1df.svg"
+  },
+  {
+    "revision": "1fc031461184d9ecd11eec32e98970c8",
+    "url": "/static/media/bsc-testnet-logo.1fc03146.png"
+  },
+  {
+    "revision": "6d62eaa3d7cacb1cf8bde6f7002fcdb6",
+    "url": "/static/media/check-icon.6d62eaa3.webp"
+  },
+  {
+    "revision": "1e81241566d8ba7b22a288c08242f780",
+    "url": "/static/media/coin.1e812415.png"
+  },
+  {
+    "revision": "5066d82f901290718d4af050266fe225",
+    "url": "/static/media/create-launchpad.5066d82f.png"
+  },
+  {
+    "revision": "a29037d665b05dd1b03e342789b9fdb7",
+    "url": "/static/media/create-token.a29037d6.png"
+  },
+  {
+    "revision": "6de42d17eea8163f4b50fe0763107e17",
+    "url": "/static/media/discord.6de42d17.jpg"
+  },
+  {
+    "revision": "00b506b2d45e46d80f46c84e2bf3d2b9",
+    "url": "/static/media/ethereum-logo.00b506b2.png"
+  },
+  {
+    "revision": "4e99aafe8248baccf9f185dee16c614b",
+    "url": "/static/media/lock.4e99aafe.png"
+  },
+  {
+    "revision": "f698a7f3e61b693ec349756ba4293ede",
+    "url": "/static/media/lock.f698a7f3.png"
+  },
+  {
+    "revision": "df85d4923ca991c967bdcec0036de9ac",
+    "url": "/static/media/logo.df85d492.jfif"
+  },
+  {
+    "revision": "b8710c152a3df222e73cbb1efd0ad8c2",
+    "url": "/static/media/multisend.b8710c15.png"
+  },
+  {
+    "revision": "f90c949fbe73327c01d0a2f6104d6d88",
+    "url": "/static/media/ropsten-testnet.f90c949f.webp"
+  },
+  {
+    "revision": "ffebadadbbf5640e7e55de58c81aa764",
+    "url": "/static/media/telegram.ffebadad.webp"
+  },
+  {
+    "revision": "1fcdfa46f0d86204197199f44c2f2a04",
+    "url": "/static/media/twitter.1fcdfa46.png"
+  },
+  {
+    "revision": "878b9b5a744d2ed4be46b13cc79a333b",
+    "url": "/static/media/under-contruction2.878b9b5a.jpg"
+  },
+  {
+    "revision": "1030ebcd739ff113fb3d76614ed923c2",
+    "url": "/static/media/walletConnectLogo.1030ebcd.webp"
+  }
+]);
